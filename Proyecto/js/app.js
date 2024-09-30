7dc0acecbf15648f7bd325042e995ee130946701
Andres sanchez <61595126+Andres-Eduardo@users.noreply.github.com>
2024-09-30 15:02:29 -0500

    Add files via upload

---
+++ Proyecto/js/app.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function(){
+    eventListener();
+});
+
+function eventListener(){
+    const mobileMenu = document.querySelector('.mobile-menu');
+
+    mobileMenu.addEventListener('click', navegacionResponsive);
+}
+
+function navegacionResponsive(){
+    const navegacion = document.querySelector('.navegacion');
+    
+    navegacion.classList.toggle('mostrar');
+    
+
+
+}
